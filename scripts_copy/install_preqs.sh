3f3c0a9a4baa28f609530015c5f38c564dbf1b60
root <root@redis0.5redis3ycsb.streamstore-pg0.apt.emulab.net>
2024-01-10 15:46:29 -0700

    Update

--- scripts_copy/install_preqs.sh
+++ scripts_copy/install_preqs.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
-
+#
 sudo apt-get update
 sudo apt-get install -y libtool autoconf automake libibverbs-dev librdmacm-dev libibumad-dev libpci-dev
-sudo apt-get install libibverbs-dev
-sudo apt-get install librdmacm-dev
-sudo apt-get install libibumad-dev
-sudo apt-get install libpci-dev
+sudo apt-get install -y libibverbs-dev
+sudo apt-get install -y librdmacm-dev
+sudo apt-get install -y libibumad-dev
+sudo apt-get install -y libpci-dev
 sudo apt install maven
 
-sudo apt-get install pkg-config
-sudo apt-get install libuv1-dev
-sudo apt install build-essential
-sudo apt install libatomic-ops-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libuv1-dev
+sudo apt install -y build-essential
+sudo apt install -y libatomic-ops-dev
+sudo apt-get install -y ifstat mpstat iostat
 
 # Navigate to the 'deps' directory
 cd ../deps
 
-# Check if the script exists and is executable
-if [ -x "update-jemalloc.sh" ]; then
-    # Execute the script
-    sudo ./update-jemalloc.sh
-else
-    echo "update-jemalloc.sh not found or is not executable"
-fi
+# Execute the script
+chmod +x update-jemalloc.sh
+/bin/sh update-jemalloc.sh
+
+cd jemalloc
+/bin/sh autogen.sh
+/bin/sh configure
+cd ..
+sudo make jemalloc
+sudo make hiredis
+sudo make hdr_histogram
+sudo make linenoise
 
 # Return to the original directory
 cd -
+
+cd ..
+cd ..
+pwd
+chmod +x src/mkreleasehdr.sh
+sudo make
 
