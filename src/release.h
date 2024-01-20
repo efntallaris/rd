883368971170238ecb40dbca4bec5127d3b58e34
root <root@redis0.entall-187081.streamstore-pg0.clemson.cloudlab.us>
2024-01-19 21:01:55 -0500

    debug on server

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "fe22738a"
-#define REDIS_GIT_DIRTY "156"
-#define REDIS_BUILD_ID "redis0.entall-187081.streamstore-pg0.clemson.cloudlab.us-1705711608"
+#define REDIS_GIT_SHA1 "a50bcc6b"
+#define REDIS_GIT_DIRTY "29"
+#define REDIS_BUILD_ID "redis0.entall-187081.streamstore-pg0.clemson.cloudlab.us-1705715441"
