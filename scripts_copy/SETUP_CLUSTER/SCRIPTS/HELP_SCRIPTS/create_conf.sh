5bdbfb5ba0e92f3f535957a65f146ef7f7dd43f0
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-01-27 17:26:58 +0200

    Update create_conf.sh

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/HELP_SCRIPTS/create_conf.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/HELP_SCRIPTS/create_conf.sh
@@ -8,9 +8,11 @@ cat <<EOT >> "${CONFIG_FILE_DIR}"
 port ${PORT}
 cluster-enabled yes
 daemonize yes
+maxclients 100000
 cluster-node-timeout 9000
 cluster-require-full-coverage no
 bind 0.0.0.0
 appendonly no
 logfile ${LOG_FILENAME}
+
 EOT
