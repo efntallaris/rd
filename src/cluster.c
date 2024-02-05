bb45b8caed205be599bc54e0efb5e077d578956d
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-02-05 05:13:13 +0200

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -6268,7 +6268,7 @@ void *migrateRDMASlotsCommandThread(void *arg) {
 
 
 
-	int chunk_size = 1366;
+	int chunk_size = 2731;
 	for(int start=7; start<number_of_arguments; start +=chunk_size){
 
 		int end = start + chunk_size;
