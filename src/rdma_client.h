b19b4b32a258ac22b84084d05af110f17d5055f0
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-02-05 10:28:00 +0200

    Update rdma_client.h

--- src/rdma_client.h
+++ src/rdma_client.h
@@ -10,15 +10,15 @@
 
 #include "zmalloc.h"
 /* Capacity of the completion queue (CQ) */
-#define CQ_CAPACITY (2048)
+#define CQ_CAPACITY (4096)
 /* MAX SGE capacity */
 #define MAX_SGE (1)
 /* MAX work requests */
-#define MAX_WR (2048)
+#define MAX_WR (4096)
 
-#define MAX_SEND_WR (2048);
+#define MAX_SEND_WR (4096);
 #define MAX_SEND_SGE (1);
-#define MAX_RECV_WR (2048);
+#define MAX_RECV_WR (4096);
 #define MAX_RECV_SGE (1);
 
 
