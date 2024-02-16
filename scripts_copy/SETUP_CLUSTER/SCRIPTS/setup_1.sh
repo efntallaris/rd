b2d9959ba259b8a1f7c1605ce81f332f2b2b3547
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-02-16 18:53:06 +0200

    Update setup_1.sh

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/setup_1.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/setup_1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
 source ./config.sh
+
+REDIS_WORKLOAD_NAME=$1
+
+
 COMMAND_PIPE="/tmp/command-input"
 sudo rm -rf ${REDIS_LOG_DIR}/*
 sudo rm -rf ${COMMAND_PIPE}
@@ -8,5 +12,5 @@ mkfifo ${COMMAND_PIPE}
 
 ./kill_scripts.sh
 ./clean_cluster.sh
-./setup_cluster.sh
+./setup_cluster.sh ${REDIS_WORKLOAD_NAME}
 ./setup_scripts.sh
