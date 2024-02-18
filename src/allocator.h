4ebfe06d6d487ae0250ea8fd4b62438c9f8edbd8
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-02-18 04:36:55 +0200

    Update allocator.h

--- src/allocator.h
+++ src/allocator.h
@@ -11,7 +11,7 @@
 //#define BLOCK_SIZE_BYTES 3 * 1024 *1024//1024//192//4000000//256//224//1024//256
 //3.5 mb
 // #define BLOCK_SIZE_BYTES 4089446
-#define BLOCK_SIZE_BYTES 5 * 1024 *1024//1024//192//4000000//256//224//1024//256
+#define BLOCK_SIZE_BYTES 4 * 1024 *1024//1024//192//4000000//256//224//1024//256
 
 typedef struct allocated_block alloc_bloc_t;
 typedef struct r_allocator r_allocator_t;
