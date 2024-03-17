c36a55af87ec65ed4599db714786d93483b8dbb4
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-03-17 08:19:41 +0200

    Update rdma_server.c

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -8,54 +8,57 @@
 #include "rdma_common.h"
 #include "zmalloc.h"
 void start_listen(struct rdma_server_info *se) {
-	int rc;
-	rc = pthread_create(&(se->listen_thread), NULL, listenThread, (void*)se);
-	if(rc)			/* could not create thread */
-	{
-		printf("\n ERROR: return code from pthread_create is %d \n", rc);
-		exit(1);
-	}
-
+         build_context(&se->context);
+         createQueuePair(&se->context);
+         server_exh_data(&se->context, se->server_port);
+         modify_qp_state_rtr(&se->context);
+         modify_qp_state_rts(&se->context);
+//      int rc;
+//      rc = pthread_create(&(se->listen_thread), NULL, listenThread, (void*)se);
+//      if(rc)                  /* could not create thread */
+//      {
+//              printf("\n ERROR: return code from pthread_create is %d \n", rc);
+//              exit(1);
+//      }
+//
 }
 
 
 
 void *listenThread(void *data) {
-	int err;
-	struct rdma_server_info *se = (struct rdma_server_info *) data;
-	// build_context(&se->context);
-	// createQueuePair(&se->context);
-	// server_exh_data(&se->context, se->server_port);
-	// modify_qp_state_rtr(&se->context);
-	// modify_qp_state_rts(&se->context);
-	return NULL;
+        int err;
+        struct rdma_server_info *se = (struct rdma_server_info *) data;
+        // build_context(&se->context);
+        // createQueuePair(&se->context);
+        // server_exh_data(&se->context, se->server_port);
+        // modify_qp_state_rtr(&se->context);
+        // modify_qp_state_rts(&se->context);
+        //return NULL;
 }
 
 
 static struct rdma_server_info default_server_ops= {
-	.server_ops={
-		.start_listen = start_listen,
-		//todo destroy ctx
-	},
-	.buffer_ops = {
-//		.rdma_send = rdma_send,
-//		.rdma_recv = rdma_recv,
-//		.wait_for_send_completion = wait_for_send_completion,
-//		.wait_for_recv_completion = wait_for_recv_completion,
-	}
+        .server_ops={
+                .start_listen = start_listen,
+                //todo destroy ctx
+        },
+        .buffer_ops = {
+//              .rdma_send = rdma_send,
+//              .rdma_recv = rdma_recv,
+//              .wait_for_send_completion = wait_for_send_completion,
+//              .wait_for_recv_completion = wait_for_recv_completion,
+        }
 };
 
 
 
 
 struct rdma_server_info *init_rdma_server(char *server_port){
-	struct rdma_server_info *s = (struct rdma_server_info *) zmalloc(sizeof(struct rdma_server_info));
-	strncpy(s->server_port, server_port, sizeof(server_port));
-
-	printf("%s server port\n", s->server_port);
-	s->server_ops = default_server_ops.server_ops;
-	s->buffer_ops = default_server_ops.buffer_ops;
-	s->server_ops.start_listen(s);
+        struct rdma_server_info *s = (struct rdma_server_info *) zmalloc(sizeof(struct rdma_server_info));
+        strncpy(s->server_port, server_port, sizeof(server_port));
+        s->server_ops = default_server_ops.server_ops;
+        s->buffer_ops = default_server_ops.buffer_ops;
+        s->server_ops.start_listen(s);
 
 
 }
