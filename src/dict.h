cded41426f28d3f291866eee5718380490671bef
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-03-18 22:01:44 +0200

    Update dict.h

--- src/dict.h
+++ src/dict.h
@@ -105,8 +105,8 @@ typedef void (dictScanBucketFunction)(void *privdata, dictEntry **bucketref);
 /* This is the initial size of every hash table */
 //#define DICT_HT_INITIAL_SIZE 16777216 
 //#define DICT_HT_INITIAL_SIZE 8388608
-#define DICT_HT_INITIAL_SIZE 2048
-#define DICT_HT_BIG_INITIAL_SIZE 67108864
+#define DICT_HT_INITIAL_SIZE 512
+#define DICT_HT_BIG_INITIAL_SIZE 536870912
 //#define DICT_HT_INITIAL_SIZE 1048576
 
 /* ------------------------------- Macros ------------------------------------*/
