6d088e10aaf53f05ebbe8e88331b5d6494c42650
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-03-18 22:02:41 +0200

    Update sync_source.sh

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
@@ -9,17 +9,16 @@ for redis_instance in "${!instances[@]}"; do
     for i in "${!nodeInstance[@]}"; do
         IFS="|" read -r -a info <<< "${nodeInstance[i]}"
         tko=$(sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
-        sudo rm -rf /root/rd/src
-        sudo rm -rf /root/rd/scripts_copy
-        sudo rm -rf /root/rd/workloads
-        cd /tmp
-        rm -rf rd
-        git clone https://github.com/efntallaris/rd
-        cp -rf rd/scripts_copy /root/rd/scripts_copy
-        cp -rf rd/src /root/rd/src
-        cp -rf rd/workloads /root/rd/workloads
-#        chmod +x install_preqs.sh
-#        ./install_preqs.sh
-
+sudo rm -rf /root/rd/src
+sudo rm -rf /root/rd/scripts_copy
+sudo rm -rf /root/rd/workloads
+cd /tmp
+rm -rf rd
+git clone https://github.com/efntallaris/rd
+cp -rf rd/scripts_copy /root/rd/scripts_copy
+cp -rf rd/src /root/rd/src
+cp -rf rd/workloads /root/rd/workloads
 EOF
 2>&1)
+    done
+done
