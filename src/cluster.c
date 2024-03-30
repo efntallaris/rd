6927f01cc462f89943228f533064e36de2ee58fc
root <root@localhost>
2024-03-29 23:07:20 -0600

    upd

--- src/cluster.c
+++ src/cluster.c
@@ -7397,7 +7397,7 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 
 	int read_command = (c->cmd->flags & CMD_READONLY) ||
 		(c->cmd->proc == execCommand && (c->mstate.cmd_flags & CMD_READONLY));
-	if(!write_command){
+	if(!importing_slot && write_command){
 		if(pthread_mutex_trylock(&server.ownership_lock_slots[slot]) == 0){
 			if(server.migration_ownership_changed[slot] == 1) {
 				server.migration_ownership_changed[slot] = 0;
@@ -7426,7 +7426,7 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 		}
 
 	}
-	if(write_command){
+	if(importing_slot && write_command){
 		if(pthread_mutex_trylock(&server.ownership_lock_slots[slot]) == 0){
 			if(server.migration_ownership_changed[slot] == 1) {
 				server.migration_ownership_changed[slot] = 0;
