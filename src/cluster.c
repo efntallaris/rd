0f9fbc4e91a40f1defb70e656c00deb0d82defb4
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-04-02 22:12:21 +0300

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -7449,10 +7449,10 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 				}
 				if(recipientNode != NULL) {
 					// serverLog(LL_WARNING, "STRATOS CHANGING OWNERSHIP TO recipientNode %s", recipientNode->name);
-				 	server.cluster->slots[slot] = recipientNode;
-				 	server.cluster->migrating_slots_to[slot] = NULL;
-				 	server.cluster->importing_slots_from[slot] = NULL;
-				 	pthread_mutex_unlock(&(server.ownership_lock_slots[slot]));
+				 	// server.cluster->slots[slot] = recipientNode;
+				 	// server.cluster->migrating_slots_to[slot] = NULL;
+				 	// server.cluster->importing_slots_from[slot] = NULL;
+				 	// pthread_mutex_unlock(&(server.ownership_lock_slots[slot]));
 				 	// return recipientNode;
 					return myself;
 				}else{
@@ -7476,18 +7476,18 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 		// serverLog(LL_WARNING, "IM HERE WRITE");
 		if(pthread_mutex_trylock(&server.ownership_lock_slots[slot]) == 0){
 			if(server.migration_ownership_changed[slot] == 1) {
-				serverLog(LL_WARNING, "STRATOS CHECKING READ FOR SLOT %d -> %d", slot, server.migration_ownership_changed[slot]);
+				// serverLog(LL_WARNING, "STRATOS CHECKING READ FOR SLOT %d -> %d", slot, server.migration_ownership_changed[slot]);
 				server.migration_ownership_changed[slot] = 0;
 				clusterNode *recipientNode = server.cluster->migrating_slots_to[slot];
 				if(error_code) {
 				 	*error_code = CLUSTER_REDIR_MOVED;
 				}
 				if(recipientNode != NULL) {
-					serverLog(LL_WARNING, "STRATOS CHANGING OWNERSHIP TO recipientNode %s", recipientNode->name);
-				 	server.cluster->slots[slot] = recipientNode;
-				 	server.cluster->migrating_slots_to[slot] = NULL;
-				 	server.cluster->importing_slots_from[slot] = NULL;
-				 	pthread_mutex_unlock(&(server.ownership_lock_slots[slot]));
+					// serverLog(LL_WARNING, "STRATOS CHANGING OWNERSHIP TO recipientNode %s", recipientNode->name);
+				 // 	server.cluster->slots[slot] = recipientNode;
+				 // 	server.cluster->migrating_slots_to[slot] = NULL;
+				 // 	server.cluster->importing_slots_from[slot] = NULL;
+				 // 	pthread_mutex_unlock(&(server.ownership_lock_slots[slot]));
 				 	// return recipientNode;
 					return myself;
 				}else{
