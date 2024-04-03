f5fd6678b039007bf1582aeac37fc8b8e5099a9d
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-04-03 13:57:32 +0300

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -7508,9 +7508,9 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 
 				server.migration_ownership_changed[slot] = 0;
 				clusterNode *recipientNode = server.cluster->migrating_slots_to[slot];
-				// if(error_code) {
-				//  	*error_code = CLUSTER_REDIR_MOVED;
-				// }
+				if(error_code) {
+				 	*error_code = CLUSTER_REDIR_MOVED;
+				}
 				if(recipientNode != NULL) {
 					clusterDelSlot(slot);
 					clusterAddSlot(recipientNode,slot);
@@ -7527,8 +7527,7 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 					
 				 	
 				 	pthread_mutex_unlock(&(server.ownership_lock_slots[slot]));
-				 	// return recipientNode;
-					return myself;
+				 	return recipientNode;
 				}else{
 					serverLog(LL_WARNING, "STRATOS RECIPIENT NODE NOT FOUND?");
 				}
@@ -7558,9 +7557,9 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 				// serverLog(LL_WARNING, "STRATOS CHECKING READ FOR SLOT %d -> %d", slot, server.migration_ownership_changed[slot]);
 				server.migration_ownership_changed[slot] = 0;
 				clusterNode *recipientNode = server.cluster->migrating_slots_to[slot];
-				// if(error_code) {
-				//  	*error_code = CLUSTER_REDIR_MOVED;
-				// }
+				if(error_code) {
+				 	*error_code = CLUSTER_REDIR_MOVED;
+				}
 				if(recipientNode != NULL) {
 					// serverLog(LL_WARNING, "STRATOS CHANGING OWNERSHIP TO recipientNode %s", recipientNode->name);
 				 	clusterDelSlot(slot);
@@ -7576,8 +7575,7 @@ clusterNode *getNodeByQuery(client *c, struct redisCommand *cmd, robj **argv, in
 							CLUSTER_TODO_UPDATE_STATE|
 							CLUSTER_TODO_FSYNC_CONFIG);
 					pthread_mutex_unlock(&server.ownership_lock_slots[slot]);
-				 	// return recipientNode;
-					return myself;
+				 	return recipientNode;
 				}else{
 					serverLog(LL_WARNING, "STRATOS RECIPIENT NODE NOT FOUND?");
 				}
