16601916a50f976ddd132600010bf8460b3de460
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-04-09 22:23:49 +0300

    Update install_preqs.sh

--- scripts_copy/install_preqs.sh
+++ scripts_copy/install_preqs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /root/rd
+git stash
 git checkout spill_over_buffers_as_chunks
 sudo apt-get update
 sudo apt-get install -y libtool autoconf automake libibverbs-dev librdmacm-dev libibumad-dev libpci-dev
