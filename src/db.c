c9258503638d5eb7631ef9d512434a5c01fd9701
root <root@redis0.entall-206095.streamstore-pg0.utah.cloudlab.us>
2024-05-07 00:11:33 -0600

    upd

--- src/db.c
+++ src/db.c
@@ -334,8 +334,8 @@ void genericSetKey(client *c, redisDb *db, robj *key, robj *val, int keepttl, in
 		//IF OWNERSHIP IS CHANGED DISCARD THE REQUEST AND RETURN TRY AGAIN TO CLIENT, CLIENT THEN WILL BE REDIRECTED TO RECIPIENT
 
 		if(server.migration_spill_over_phase_activated[hashSlot] == 1){
+			serverLog(LL_WARNING, "STRATOS IM HERE");
 			unsigned long spill_over_slot = getSpillOverSlot(server.cluster->myself->ip, 16386);
-			//serverLog(LL_WARNING, "STRATOS SPILL OVER SLOT [ADD] IS:%d", spill_over_slot);
 			r_allocator_insert_kv(spill_over_slot,
 					(char *)key->ptr-8, sdslen(key->ptr)+ 8 + 1,
 					(char *)val->ptr-8, sdslen(val->ptr)+ 8 + 1,
--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "3b598903"
-#define REDIS_GIT_DIRTY "0"
-#define REDIS_BUILD_ID "redis0.entall-206095.streamstore-pg0.utah.cloudlab.us-1715055930"
+#define REDIS_GIT_SHA1 "ae478d2e"
+#define REDIS_GIT_DIRTY "257"
+#define REDIS_BUILD_ID "redis0.entall-206095.streamstore-pg0.utah.cloudlab.us-1715062281"
