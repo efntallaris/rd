d7fdcf6e5205feb87d9ad5b22e1f46948cd6ef28
root <root@redis0.entall-206095.streamstore-pg0.utah.cloudlab.us>
2024-05-07 00:49:22 -0600

    upd

--- src/db.c
+++ src/db.c
@@ -335,15 +335,15 @@ void genericSetKey(client *c, redisDb *db, robj *key, robj *val, int keepttl, in
 
 		if(server.migration_spill_over_phase_activated[hashSlot] == 1){
 			serverLog(LL_WARNING, "STRATOS IM HERE");
-			unsigned long spill_over_slot = getSpillOverSlot(server.cluster->myself->ip, 16386);
-			r_allocator_insert_kv(spill_over_slot,
-					(char *)key->ptr-8, sdslen(key->ptr)+ 8 + 1,
-					(char *)val->ptr-8, sdslen(val->ptr)+ 8 + 1,
-					key, sizeof(robj),
-					val, sizeof(robj),
-					&allocated_block,
-					&allocator_key,
-					&allocator_value);
+//			unsigned long spill_over_slot = getSpillOverSlot(server.cluster->myself->ip, 16386);
+//			r_allocator_insert_kv(spill_over_slot,
+//					(char *)key->ptr-8, sdslen(key->ptr)+ 8 + 1,
+//					(char *)val->ptr-8, sdslen(val->ptr)+ 8 + 1,
+//					key, sizeof(robj),
+//					val, sizeof(robj),
+//					&allocated_block,
+//					&allocator_key,
+//					&allocator_value);
 		}else{
 			r_allocator_insert_kv(hashSlot,
 					(char *)key->ptr-8, sdslen(key->ptr)+ 8 + 1,
--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "ae478d2e"
-#define REDIS_GIT_DIRTY "257"
-#define REDIS_BUILD_ID "redis0.entall-206095.streamstore-pg0.utah.cloudlab.us-1715062281"
+#define REDIS_GIT_SHA1 "c9258503"
+#define REDIS_GIT_DIRTY "29"
+#define REDIS_BUILD_ID "redis0.entall-206095.streamstore-pg0.utah.cloudlab.us-1715063522"
