b59f43f612e0b1d2f9daaa7df7649c0858e509cb
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-05-25 21:46:04 +0300

    Update allocator.c

--- src/allocator.c
+++ src/allocator.c
@@ -960,11 +960,11 @@ void traverse_print_slot_blocks_filename(int slot, const char *filename)
 	slots_bytes_free = cur_block->bytes_free;
         cur_block = cur_block->next;
     }
-    size_t total_blocks = total_used_bytes/slots_bytes_free;
+    float total_blocks = (float)total_used_bytes / slots_bytes_free;
     // int total_blocks = 2 ;
     size_t rest_free_bytes_on_last_slot = total_used_bytes - ((total_blocks - 1) * slots_bytes_free);
     rest_free_bytes_on_last_slot = slots_bytes_free - rest_free_bytes_on_last_slot;
-    fprintf(file, "Total Blocks:%d, Total Segments:%zu, Last Slot unused bytes:%zu. Actual bytes free per block:%zu, total bytes of slot:%zu\n", total_blocks, total_segments, rest_free_bytes_on_last_slot, slots_bytes_free, total_used_bytes);
+    fprintf(file, "Total Blocks:%f, Total Segments:%zu, Last Slot unused bytes:%zu. Actual bytes free per block:%zu, total bytes of slot:%zu\n", total_blocks, total_segments, rest_free_bytes_on_last_slot, slots_bytes_free, total_used_bytes);
     fprintf(file, "\n");
 
     fclose(file);
