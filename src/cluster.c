29435496ced0eb39ad89cec857df88c4b38fc167
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-05-27 05:53:58 +0300

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -6220,14 +6220,14 @@ void *migrateRDMASlotsCommandThread(void *arg) {
 
 	if(strcmp(myself->ip, "10.10.1.2") == 0) {
 		serverLog(LL_WARNING, "STRATOS SLEEPING FOR 60 SECONDS");
-		sleep(100);
+		sleep(40);
 		//sleep(70);
 		//sleep(200);
 	}
 
 	if(strcmp(myself->ip, "10.10.1.3") == 0) {
 		serverLog(LL_WARNING, "STRATOS SLEEPING FOR 60 SECONDS");
-		sleep(180);
+		sleep(60);
 		//sleep(140);
 		//sleep(400);
 	}
