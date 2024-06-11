ea5037ca90f64ae621b15be9067cc98ecb10a667
root <root@localhost>
2024-06-11 04:59:01 -0600

    upd

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
@@ -9,15 +9,9 @@ for redis_instance in "${!instances[@]}"; do
     for i in "${!nodeInstance[@]}"; do
         IFS="|" read -r -a info <<< "${nodeInstance[i]}"
         tko=$(sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
-sudo rm -rf /root/rd/src
-sudo rm -rf /root/rd/scripts_copy
-sudo rm -rf /root/rd/workloads
-cd /tmp
-rm -rf rd
-git clone https://github.com/efntallaris/rd
-cp -rf rd/scripts_copy /root/rd/scripts_copy
-cp -rf rd/src /root/rd/src
-cp -rf rd/workloads /root/rd/workloads
+cd /root/rd/
+git pull origin vanilla_redis
+git checkout vanilla_redis
 EOF
 2>&1)
     done
