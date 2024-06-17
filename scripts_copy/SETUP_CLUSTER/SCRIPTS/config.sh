5f6544c1757461d985d98b2690d7e97cf42a0ff1
root <root@localhost>
2024-06-17 16:08:18 -0600

    upd

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
@@ -20,7 +20,7 @@ REDIS_WORKLOAD_PATH="${MAIN_DIR}/workloads/"
 YCSB_LOG_FILENAME="/tmp/ycsb_output"
 LOCAL_LOG_DIR="/root/systat_logs"
 
-EXPERIMENT_NAME="3CHUNKS"
+EXPERIMENT_NAME="1CHUNK"
 
 #	MASTER NODES 	#
 declare -A redis_master_instances 
--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "f53589bb"
+#define REDIS_GIT_SHA1 "ff7164dc"
 #define REDIS_GIT_DIRTY "29"
-#define REDIS_BUILD_ID "redis0.entall-209826.streamstore-pg0.utah.cloudlab.us-1718651085"
+#define REDIS_BUILD_ID "redis0.entall-209826.streamstore-pg0.utah.cloudlab.us-1718660881"
