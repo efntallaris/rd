a47560d15f0c66616355ec4ab83d6cb9566a2906
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-07-09 12:05:21 +0300

    Update dict.c

--- src/dict.c
+++ src/dict.c
@@ -483,10 +483,10 @@ dictEntry *dictAddRaw(dict *d, void *key, dictEntry **existing)
 
 	char buffer1[4096];
 	dictGetStats(buffer1,	sizeof(buffer1),d);
-	log_file = fopen("/tmp/dictlog", "a");  // Open file in append mode
+	//log_file = fopen("/tmp/dictlog", "a");  // Open file in append mode
 	// fprintf(log_file, "HT : %s\n", buffer1);
-	fprintf(log_file, "adding key : %s, INDEX:%ld, HASH:%ld TimeIndex:%f\n", (char *) key, index, dictHashKey(d, key), time_taken);
-	fclose(log_file);
+	//fprintf(log_file, "adding key : %s, INDEX:%ld, HASH:%ld TimeIndex:%f\n", (char *) key, index, dictHashKey(d, key), time_taken);
+	//fclose(log_file);
 
 //	log_file = fopen("/tmp/dictlog", "a");  // Open file in append mode
 //	fprintf(log_file, "adding key : %s, INDEX:%ld, HASH:%ld\n", (char *) key, index, dictHashKey(d, key));
