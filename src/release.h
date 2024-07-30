98a2685209e37015c93818208cc8b0c8d146f6c1
root <root@localhost>
2024-07-29 21:22:22 -0600

    upx

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "857544b9"
-#define REDIS_GIT_DIRTY "66"
-#define REDIS_BUILD_ID "redis0.entall-212710.streamstore-pg0.utah.cloudlab.us-1722307369"
+#define REDIS_GIT_SHA1 "1a3fd00a"
+#define REDIS_GIT_DIRTY "458"
+#define REDIS_BUILD_ID "redis0.entall-212710.streamstore-pg0.utah.cloudlab.us-1722309727"
