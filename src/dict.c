03195a3615518c1b443424d11c06ee74cb0c69fb
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-10-22 01:15:30 +0300

    Update dict.c

--- src/dict.c
+++ src/dict.c
@@ -49,7 +49,6 @@
 #include <time.h>
 // Migration variables
 static FILE *log_file;
-static int migration_activated = 0;
 static pthread_mutex_t* migration_dict_locks; 
 static dict *target_db;
 
@@ -1241,15 +1240,6 @@ void dictEmpty(dict *d, void(callback)(void*)) {
 	_dictClear(d,&d->ht[1],callback);
 	d->rehashidx = -1;
 	d->pauserehash = 0;
-}
-
-
-void dictEnableMigration(void) {
-	migration_activated = 1;
-}
-
-void dictDisableMigration(void) {
-	migration_activated = 0;
 }
 
 void dictEnableResize(void) {
