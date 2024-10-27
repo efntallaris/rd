641501ef1acf77ef198796e6f36654c3e06cdf45
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-10-27 03:03:07 +0200

    Update networking.c

--- src/networking.c
+++ src/networking.c
@@ -113,11 +113,7 @@ static void clientSetDefaultAuth(client *c) {
                        !(c->user->flags & USER_FLAG_DISABLED);
 }
 
-client *createClient(connection *conn) {
-    if(read_query_mutex == NULL){
-	pthread_mutex_init(&read_query_mutex, NULL);
-    }
-	
+client *createClient(connection *conn) {	
     client *c = zmalloc(sizeof(client));
 
     /* passing NULL as conn it is possible to create a non connected client.
@@ -372,7 +368,7 @@ void _addReplyProtoToList(client *c, const char *s, size_t len) {
 
         closeClientOnOutputBufferLimitReached(c, 1);
     }
-    pthread_mutex_unlock(&(server.socket_mutex));
+    pthread_mutex_unlock(&server.socket_mutex);
 }
 
 /* -----------------------------------------------------------------------------
@@ -425,7 +421,7 @@ void addReplySds(client *c, sds s) {
  * in the list of objects. */
 void addReplyProto(client *c, const char *s, size_t len) {
     if (prepareClientToWrite(c) != C_OK){
-    	    pthread_mutex_unlock(&(server.socket_mutex));
+    	    pthread_mutex_unlock(&server.socket_mutex);
 	    return;
     }
     if (_addReplyToBuffer(c,s,len) != C_OK)
@@ -2169,14 +2165,17 @@ void processInputBuffer(client *c) {
 }
 
 void readQueryFromClient(connection *conn) {
+    pthread_mutex_lock(&server.socket_mutex);
     client *c = connGetPrivateData(conn);
     int nread, readlen;
     size_t qblen;
 
     /* Check if we want to read from the client later when exiting from
      * the event loop. This is the case if threaded I/O is enabled. */
-    if (postponeClientRead(c)) return;
-
+    if (postponeClientRead(c)){
+	    pthread_mutex_unlock(&server.socket_mutex);
+	    return;
+    }
     /* Update total number of reads on server */
     atomicIncr(server.stat_total_reads_processed, 1);
 
@@ -2203,15 +2202,18 @@ void readQueryFromClient(connection *conn) {
     nread = connRead(c->conn, c->querybuf+qblen, readlen);
     if (nread == -1) {
         if (connGetState(conn) == CONN_STATE_CONNECTED) {
+	    pthread_mutex_unlock(&server.socket_mutex);
             return;
         } else {
             serverLog(LL_VERBOSE, "Reading from client: %s",connGetLastError(c->conn));
             freeClientAsync(c);
+	    pthread_mutex_unlock(&server.socket_mutex);
             return;
         }
     } else if (nread == 0) {
         serverLog(LL_VERBOSE, "Client closed connection");
         freeClientAsync(c);
+	pthread_mutex_unlock(&server.socket_mutex);
         return;
     } else if (c->flags & CLIENT_MASTER) {
         /* Append the query buffer to the pending (not applied) buffer
@@ -2233,6 +2235,7 @@ void readQueryFromClient(connection *conn) {
         sdsfree(ci);
         sdsfree(bytes);
         freeClientAsync(c);
+	pthread_mutex_unlock(&server.socket_mutex);
         return;
     }
 
@@ -2240,6 +2243,7 @@ void readQueryFromClient(connection *conn) {
      * in case to check if there is a full command to execute. */
     //serverLog(LL_WARNING, "STRATOS IN READQUERYFROMCLIENT");
     processInputBuffer(c);
+    pthread_mutex_unlock(&server.socket_mutex);
 }
 
 void getClientsMaxBuffers(unsigned long *longest_output_list,
