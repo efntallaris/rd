2a3d76ca06db2079c75c964c0f516464bf439f5b
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-11-14 05:37:17 +0200

    Update config.sh

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
@@ -20,7 +20,7 @@ REDIS_WORKLOAD_PATH="${MAIN_DIR}/workloads/"
 YCSB_LOG_FILENAME="/tmp/ycsb_output"
 LOCAL_LOG_DIR="/root/systat_logs"
 
-EXPERIMENT_NAME="512kb_block"
+EXPERIMENT_NAME="512kb_block_new"
 
 #	MASTER NODES 	#
 declare -A redis_master_instances 
