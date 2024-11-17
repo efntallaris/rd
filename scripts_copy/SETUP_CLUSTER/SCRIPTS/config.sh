29c9d58d5fe5048ed6eef65dc8ebda6923a6fca8
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2024-11-17 17:26:51 +0200

    Update config.sh

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/config.sh
@@ -20,7 +20,7 @@ REDIS_WORKLOAD_PATH="${MAIN_DIR}/workloads/"
 YCSB_LOG_FILENAME="/tmp/ycsb_output"
 LOCAL_LOG_DIR="/root/systat_logs"
 
-EXPERIMENT_NAME="2CHUNKS"
+EXPERIMENT_NAME="99th_latency"
 
 #	MASTER NODES 	#
 declare -A redis_master_instances 
