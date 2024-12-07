6a5c121f331fef09ab1ebd7f12c56192ca92728f
entallaris <ef.ntallaris@gmail.com>
2024-12-07 08:03:41 +0200

    update

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/build_ycsb.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/build_ycsb.sh
@@ -6,24 +6,23 @@ YCSB_INTERMEDIATE_FOLDER="/tmp/ycsb_temp"
 YCSB_BIN="/root/ycsb_client"
 YCSB_BIN=$2
 
-
 sudo rm -rf ${YCSB_INTERMEDIATE_FOLDER}
 sudo mkdir -p ${YCSB_INTERMEDIATE_FOLDER}
 
 cd ${YCSB_DIR}
-sudo mvn -pl redis -am clean install -T 9 -DskipTests -Dcheckstyle.skip
-
+#sudo mvn -pl redis -am clean install -T 9 -DskipTests -Dcheckstyle.skip
+sudo mvn -pl site.ycsb:redis-binding -am clean install -T 9 -DskipTests -Dcheckstyle.skip
 
 cd ${YCSB_DIR}/redis/target
-sudo tar -xvf ycsb-redis-binding-0.17.0.tar.gz --directory ${YCSB_INTERMEDIATE_FOLDER}
+sudo tar -xvf ycsb-redis-binding-0.18.0.tar.gz --directory ${YCSB_INTERMEDIATE_FOLDER}
 
 # Check if the directory exists
 if [ -d "$YCSB_BIN" ]; then
-    # Directory exists, delete everything inside it
-    rm -rf "$YCSB_BIN"/*
+  # Directory exists, delete everything inside it
+  rm -rf "$YCSB_BIN"/*
 else
-    # Directory does not exist, so create it
-    mkdir -p "$YCSB_BIN"
+  # Directory does not exist, so create it
+  mkdir -p "$YCSB_BIN"
 fi
 
-sudo cp -rf ${YCSB_INTERMEDIATE_FOLDER}/ycsb-redis-binding-0.17.0/*  ${YCSB_BIN}
+sudo cp -rf ${YCSB_INTERMEDIATE_FOLDER}/ycsb-redis-binding-0.18.0/* ${YCSB_BIN}
