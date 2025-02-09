b58184d030451deb4350263d2ac67bc612d176d0
Your Name <you@example.com>
2025-02-09 05:19:56 +0200

    debug version of ht

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "1dcced76"
-#define REDIS_GIT_DIRTY "28"
-#define REDIS_BUILD_ID "redis0.entall-216038.streamstore-pg0.utah.cloudlab.us-1725625943"
+#define REDIS_GIT_SHA1 "43d11756"
+#define REDIS_GIT_DIRTY "333"
+#define REDIS_BUILD_ID "stratos-pc-1739071173"
