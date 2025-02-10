eaf615ee36d045d9b38f8014ecdd587636dc60e5
Your Name <you@example.com>
2025-02-10 12:33:13 +0200

    HASH TABLE = 4

--- src/cluster.c
+++ src/cluster.c
@@ -7175,6 +7175,7 @@ void *rdmaDoneBatchThreadFunc(void *arg) {
         }
 
         if(firstSlot == lastSlot && thread_id!=0){
+			    serverLog(LL_WARNING, "STRATOS FIRSTSLOT IS THE SAME AS LASTSLOT %s", item->first_slot);
           continue;
         }
 
