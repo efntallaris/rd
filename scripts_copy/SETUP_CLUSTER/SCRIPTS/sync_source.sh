1ac6468939c83f11a5222b2d2df00404e4fff6a8
Your Name <you@example.com>
2025-02-16 11:54:02 +0200

    non blocking first commit

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
@@ -4,18 +4,20 @@ source ./config.sh
 rm -rf ${YCSB_LOG_FILENAME}
 rm -rf ${LOCAL_LOG_DIR}
 for redis_instance in "${!instances[@]}"; do
-    echo "$redis_instance - ${instances[$redis_instance]}"
-    IFS=',' read -r -a nodeInstance <<< "${instances[$redis_instance]}"
-    for i in "${!nodeInstance[@]}"; do
-        IFS="|" read -r -a info <<< "${nodeInstance[i]}"
-        tko=$(sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
+  echo "$redis_instance - ${instances[$redis_instance]}"
+  IFS=',' read -r -a nodeInstance <<<"${instances[$redis_instance]}"
+  for i in "${!nodeInstance[@]}"; do
+    IFS="|" read -r -a info <<<"${nodeInstance[i]}"
+    tko=$(
+      sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
 cd /root/rd
 git stash 
-git pull origin blocking_version
-git checkout -b blocking_version
+git pull origin non_blocking_version
+git checkout -b non_blocking_version
 cd /root/rd/scripts_copy
 #./install_preqs.sh
 EOF
-2>&1)
-    done
+      2>&1
+    )
+  done
 done
