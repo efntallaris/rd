2275265810d045c9e605b6fd30fd73a22b36e509
entallaris <ef.ntallaris@gmail.com>
2025-07-15 15:41:22 +0300

    new sync_source

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
@@ -1,24 +1,39 @@
 #!/bin/bash -e
 
+# Default branch if no argument is passed
+DEFAULT_BRANCH="blocking_version"
+BRANCH_NAME=${1:-$DEFAULT_BRANCH}
+
 source ./config.sh
-rm -rf ${YCSB_LOG_FILENAME}
-rm -rf ${LOCAL_LOG_DIR}
+
+rm -rf "${YCSB_LOG_FILENAME}"
+rm -rf "${LOCAL_LOG_DIR}"
+
 for redis_instance in "${!instances[@]}"; do
-    echo "$redis_instance - ${instances[$redis_instance]}"
-    IFS=',' read -r -a nodeInstance <<< "${instances[$redis_instance]}"
-    for i in "${!nodeInstance[@]}"; do
-        IFS="|" read -r -a info <<< "${nodeInstance[i]}"
-        tko=$(sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
-sudo rm -rf /root/rd/src
-sudo rm -rf /root/rd/scripts_copy
-sudo rm -rf /root/rd/workloads
-cd /tmp
-rm -rf rd
-git clone https://github.com/efntallaris/rd
-cp -rf rd/scripts_copy /root/rd/scripts_copy
-cp -rf rd/src /root/rd/src
-cp -rf rd/workloads /root/rd/workloads
+  echo "$redis_instance - ${instances[$redis_instance]}"
+  IFS=',' read -r -a nodeInstance <<<"${instances[$redis_instance]}"
+
+  for i in "${!nodeInstance[@]}"; do
+    IFS="|" read -r -a info <<<"${nodeInstance[i]}"
+
+    sudo ssh -o StrictHostKeyChecking=no "${info[1]}" bash <<EOF
+set -e
+
+BRANCH="${BRANCH_NAME}"
+
+# Clone repo if missing
+if [ ! -d /root/rd/.git ]; then
+    rm -rf /root/rd
+    git clone https://github.com/efntallaris/rd /root/rd
+fi
+
+cd /root/rd
+
+# Sync to specified branch
+git fetch origin
+git checkout -B "\$BRANCH" "origin/\$BRANCH"
+git pull origin "\$BRANCH"
 EOF
-2>&1)
-    done
+
+  done
 done
