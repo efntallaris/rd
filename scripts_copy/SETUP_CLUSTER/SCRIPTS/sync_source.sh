c9bda1eba402ebb952bd80d0c08b67068c6cca2f
entallaris <ef.ntallaris@gmail.com>
2025-07-15 15:42:43 +0300

    new sync_source

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/sync_source.sh
@@ -1,23 +1,39 @@
 #!/bin/bash -e
 
+# Default branch if no argument is passed
+DEFAULT_BRANCH="redis_fulva_protocol"
+BRANCH_NAME=${1:-$DEFAULT_BRANCH}
+
 source ./config.sh
-rm -rf ${YCSB_LOG_FILENAME}
-rm -rf ${LOCAL_LOG_DIR}
+
+rm -rf "${YCSB_LOG_FILENAME}"
+rm -rf "${LOCAL_LOG_DIR}"
+
 for redis_instance in "${!instances[@]}"; do
   echo "$redis_instance - ${instances[$redis_instance]}"
   IFS=',' read -r -a nodeInstance <<<"${instances[$redis_instance]}"
+
   for i in "${!nodeInstance[@]}"; do
     IFS="|" read -r -a info <<<"${nodeInstance[i]}"
-    tko=$(
-      sudo ssh -o StrictHostKeyChecking=no ${info[1]} bash <<EOF
+
+    sudo ssh -o StrictHostKeyChecking=no "${info[1]}" bash <<EOF
+set -e
+
+BRANCH="${BRANCH_NAME}"
+
+# Clone repo if missing
+if [ ! -d /root/rd/.git ]; then
+    rm -rf /root/rd
+    git clone https://github.com/efntallaris/rd /root/rd
+fi
+
 cd /root/rd
-git stash 
-git pull origin non_blocking_version
-git checkout -b non_blocking_version
-cd /root/rd/scripts_copy
-#./install_preqs.sh
+
+# Sync to specified branch
+git fetch origin
+git checkout -B "\$BRANCH" "origin/\$BRANCH"
+git pull origin "\$BRANCH"
 EOF
-      2>&1
-    )
+
   done
 done
