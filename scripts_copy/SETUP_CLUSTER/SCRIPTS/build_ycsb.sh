529718119bcc0f89fe2b61a43e582142f2178f75
entallaris <ef.ntallaris@gmail.com>
2025-07-17 06:47:07 +0300

    changd ycsb client to use Lettuce

--- scripts_copy/SETUP_CLUSTER/SCRIPTS/build_ycsb.sh
+++ scripts_copy/SETUP_CLUSTER/SCRIPTS/build_ycsb.sh
@@ -9,6 +9,9 @@ YCSB_BIN=$2
 sudo rm -rf ${YCSB_INTERMEDIATE_FOLDER}
 sudo mkdir -p ${YCSB_INTERMEDIATE_FOLDER}
 
+cd /root/rd/lettuce
+sudo mvn clean install -DskipTests
+
 cd ${YCSB_DIR}
 sudo mvn -pl redis -am clean install -T 9 -DskipTests -Dcheckstyle.skip
 
