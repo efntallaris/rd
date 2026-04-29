3986de556f8a4d4a79edcbd9a75b9137a61969b3
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2026-04-29 15:30:35 +0300

    Update install_preqs.sh

--- scripts_copy/install_preqs.sh
+++ scripts_copy/install_preqs.sh
@@ -74,6 +74,8 @@ pwd
 chmod +x src/mkreleasehdr.sh
 #sudo rm -rf /usr/local/include/infiniband
 #sudo cp -rf /usr/include/infiniband /usr/local/include
+cd /users/entall
+git clone https://github.com/efntallaris/rd
 
 sudo make
 
